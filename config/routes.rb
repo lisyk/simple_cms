6993c8899c637b413b4cd45d85c16445ccf2f721
Lisyk <sergey.liskovoy@gmail.com>
2015-04-14 14:11:47 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+  root 'demo#index'
     
+    match '/:controller(/:action(/:id))', :via => :get #!!!! need to be very last!!!
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
