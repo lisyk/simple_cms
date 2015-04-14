1d7b0957c04cb8b65aea020cb18f3c4987a3d926
Lisyk <sergey.liskovoy@gmail.com>
2015-04-14 14:35:05 -0400

    redirect and render

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,20 @@
 class DemoController < ApplicationController
-  def index
-  end
+    def index
+        render ('hello')
+    end
+    
+    def hello
+    end
+    
+    def other_hello
+        #whole command for redirect
+        #redirect_to(:controller => 'demo', :action => 'hello')
+        #short command for redirect
+        redirect_to('hello')
+    end
+    
+    def lynda
+        #external link for redirection
+        redirect_to("http://lynda.com")
+    end
 end
