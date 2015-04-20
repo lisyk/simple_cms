df1c7d4d349fcf1f9f51a71a2a05fda4c2568870
Lisyk <sergey.liskovoy@gmail.com>
2015-04-20 08:19:28 -0400

    add pages and sections

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,12 @@
 class Page < ActiveRecord::Base
-    
+    has_many :sections
     belongs_to :subject
     has_and_belongs_to_many :editors, :class_name => "AdminUser" 
     # above statement we use to change :admin_users to : editors from below statement 
 #    has_and_belongs_to_many :admin_users
     
+    scope :visible, lambda { where(:visible => true) }
+    scope :invisible, lambda { where(:visible => false) }
+    scope :sorted, lambda { order("pages.position ASC") }
+    scope :newest_first, lambda { order("pages.created_at DESC")}
 end
